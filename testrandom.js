e95e0c35873daa4bca16d161ddf7687950b8dc16
ShaunFerns <shaun.ferns@itb.ie>
2018-08-29 13:07:48 +0100

    Update testrandom.js

--- testrandom.js
+++ testrandom.js
@@ -1,17 +1,24 @@
-<!--
-   ************************************************************************************
-   ** This file has been created with the Rotating Content Tool from Amesbury Web.   **
-   ** For more information, visit us on the web:                                     **
-   **                                                                                **
-   **     Rotating Content Tool   -- http://rotatecontent.com/                       **
-   **     Company: Amesbury Web   -- http://amesburyweb.com/                         **
-   **     Author:  Randy Hoyt     -- http://randyhoyt.com/                           **
-   **                                                                                **
-   ************************************************************************************
--->
+// ************************************************************************************
+// ** This file has been created with the Rotating Content Tool by Amesbury Web.     **
+// ** For more information, visit us on the web:                                     **
+// **                                                                                **
+// **     Rotating Content Tool   -- http://rotatecontent.com/                       **
+// **     Company: Amesbury Web   -- http://amesburyweb.com/                         **
+// **     Author:  Randy Hoyt     -- http://randyhoyt.com/                           **
+// **                                                                                **
+// ************************************************************************************
 
+varLength = 2
+var entryContent = new Array(varLength)
 
-<html><head><title>Rotating Content Template</title><script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/2F340999-42FB-564A-B107-00579D0F87B1/main.js" charset="UTF-8"></script></head><body><table border="1">
-<tr><td style="vertical-align: top;">random</td><td  style="vertical-align: top; width: 600px;"><img src="https://sd-discovery.org/resources/Pictures/cropped-gamedesign-bg.jpg" alt="games" height="200" width="200"></td></tr>
-<tr><td style="vertical-align: top;">random</td><td  style="vertical-align: top; width: 600px;"><img src="https://i.ytimg.com/vi/TOQTZ6N_eVg/maxresdefault.jpg" alt="games" height="200" width="200"</td></tr>
-</table></body></html>
+entryContent[0] = "<img src=\"https://sd-discovery.org/resources/Pictures/cropped-gamedesign-bg.jpg\" alt=\"games\" height=\"200\" width=\"200\">"
+entryContent[1] = "<img src=\"https://i.ytimg.com/vi/TOQTZ6N_eVg/maxresdefault.jpg\" alt=\"games\" height=\"200\" width=\"200\""
+
+var randomNumber = Math.random()
+randomNumber *= varLength
+randomNumber = parseInt(randomNumber)
+if(isNaN(randomNumber)) randomNumber = 0
+else randomNumber %= varLength
+selectedContent = entryContent[randomNumber]
+
+document.write (selectedContent)
