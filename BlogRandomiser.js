6a5b0316216c944e512f3c7a2102fe0aa30b8aed
ShaunFerns <shaun.ferns@itb.ie>
2018-09-21 14:37:09 +0100

    Add files via upload

--- BlogRandomiser.js
+++ BlogRandomiser.js
@@ -8,15 +8,11 @@
 // **                                                                                **
 // ************************************************************************************
 
-varLength = 4
+varLength = 2
 var entryContent = new Array(varLength)
 
-entryContent[0] = "http://b00102923.blogspot.com/"
-entryContent[1] = "https://smithniamh27.blogspot.com/"
-entryContent[2] = "https://cdmmultimedia.blogspot.com/"
-entryContent[3] = "https://conorbrennan98.blogspot.com/"
-entryContent[3] = <a href="http://b00102923.blogspot.com/">Creative digital media 2018</a>
-
+entryContent[0] = "<img src=\"https://sd-discovery.org/resources/Pictures/cropped-gamedesign-bg.jpg\" alt=\"games\" height=\"200\" width=\"200\">"
+entryContent[1] = "<img src=\"https://i.ytimg.com/vi/TOQTZ6N_eVg/maxresdefault.jpg\" alt=\"games\" height=\"200\" width=\"200\""
 
 var randomNumber = Math.random()
 randomNumber *= varLength
