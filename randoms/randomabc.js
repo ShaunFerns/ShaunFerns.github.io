d9baf0f3cd9503d9c5044edbccf76de3184a37fa
ShaunFerns <shaun.ferns@tudublin.ie>
2020-07-13 16:05:43 +0100

    intial

---
+++ randoms/randomabc.js
@@ -0,0 +1,25 @@
+// ************************************************************************************
+// ** This file has been created with the Rotating Content Tool by Amesbury Web.     **
+// ** For more information, visit us on the web:                                     **
+// **                                                                                **
+// **     Rotating Content Tool   -- http://rotatecontent.com/                       **
+// **     Company: Amesbury Web   -- http://amesburyweb.com/                         **
+// **     Author:  Randy Hoyt     -- http://randyhoyt.com/                           **
+// **                                                                                **
+// ************************************************************************************
+
+varLength = 3
+var entryContent = new Array(varLength)
+
+entryContent[0] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/people-1.png\" "
+entryContent[1] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/planet-1.png\" "
+entryContent[2] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/partnership-1.png\" "
+
+var randomNumber = Math.random()
+randomNumber *= varLength
+randomNumber = parseInt(randomNumber)
+if(isNaN(randomNumber)) randomNumber = 0
+else randomNumber %= varLength
+selectedContent = entryContent[randomNumber]
+
+document.write (selectedContent)
