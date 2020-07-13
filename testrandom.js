69f8210e4f7fd4a1a020ff7b3f18fef69c86f3e9
ShaunFerns <shaun.ferns@tudublin.ie>
2020-07-13 15:58:55 +0100

    Update testrandom.js

--- testrandom.js
+++ testrandom.js
@@ -12,8 +12,8 @@ varLength = 3
 var entryContent = new Array(varLength)
 
 entryContent[0] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/people-1.png\" "
-entryContent[1] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/planet-1.png\" alt=\"Planet Card\" height=\"200\" width=\"200\""
-entryContent[2] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/partnership-1.png\" alt=\"Partnership Card\" height=\"200\" width=\"200\""
+entryContent[1] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/planet-1.png\" "
+entryContent[2] = "<img src=\"https://cdmonline.ie/cocreate/wp-content/uploads/sites/8/2020/07/partnership-1.png\" "
 
 var randomNumber = Math.random()
 randomNumber *= varLength
